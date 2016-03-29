fedb186ab4fc92ffc5ff0e49439bd1b30f32c46f
Ozan Çağlayan <ozancag@gmail.com>
2016-03-29 23:59:15 +0300

    add img->text iterator for flickr

--- nmtpy/iterators/__init__.py
+++ nmtpy/iterators/__init__.py
@@ -1,4 +1,4 @@
-import iter_text, iter_bitext, iter_imgfeats, iter_multi
+import iter_text, iter_bitext, iter_imgfeats, iter_multi, iter_flickr
 
 def get_iterator(name):
     iters = {
@@ -6,5 +6,6 @@ def get_iterator(name):
                 "bitext"    : iter_bitext.BiTextIterator,
                 "img_feats" : iter_imgfeats.ImageFeatsIterator,
                 "multi"     : iter_multi.MultiIterator,
+                "flickr"    : iter_flickr.IterFlickr,
             }
     return iters[name]
