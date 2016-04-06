59d4855c2fad46fcfe03b579a9503a4fefa8fadb
Ozan Caglayan <ozancag@gmail.com>
2016-04-06 19:41:39 +0200

    Add namedtuple Sample type

--- nmtpy/typedef.py
+++ nmtpy/typedef.py
@@ -1,2 +1,12 @@
 INT   = 'int64'
 FLOAT = 'float32'
+
+from collections import namedtuple
+
+# A simple type to hold parallel corpora samples
+# src: List of source sentence words or None if not applicable
+# trg: List of target sentence/description words
+# split: A split index starting from 0 for multiple source/target pairs
+# imgid: Index into the image feature matrix or None if not applicable
+# imgname: Filename of the image or None if not applicable
+Sample = namedtuple('Sample', ['src', 'trg', 'split', 'imgid', 'imgname'])
