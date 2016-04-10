ba1c4f79a4923128584ea6516877221d707bb2c9
Ozan Çağlayan <ozancag@gmail.com>
2016-04-10 22:48:37 +0300

    Normalize validation loss if possible

--- nmtpy/models/basemodel.py
+++ nmtpy/models/basemodel.py
@@ -96,7 +96,11 @@ class BaseModel(object):
 
         # dict of x, x_mask, y, y_mask
         for data in self.valid_iterator:
-            probs.extend(self.f_log_probs(*data.values()))
+            # Don't fail if data doesn't contain y_mask. The loss won't
+            # be normalized but the training will continue
+            norm = data['y_mask'].sum(0) if 'y_mask' in data else 1
+            log_probs = self.f_log_probs(*data.values()) / norm
+            probs.extend(log_probs)
 
         return np.array(probs).mean()
 
