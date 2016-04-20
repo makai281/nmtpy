f4a15efeb69adb7c82cd3dde87d12535858409bd
Ozan Çağlayan <ozancag@gmail.com>
2016-04-20 12:36:09 +0300

    add new gru_cond with multiple contexts

--- nmtpy/layers.py
+++ nmtpy/layers.py
@@ -69,11 +69,12 @@ def dropout_layer(state_before, use_dropout, dropout_prob, trng):
 def get_new_layer(name):
     # Layer type: (initializer, layer)
     layers = {
-                'ff'        : ('param_init_fflayer'     , 'fflayer'),
-                'gru'       : ('param_init_gru'         , 'gru_layer'),
-                'gru_cond'  : ('param_init_gru_cond'    , 'gru_cond_layer'),
-                'lstm'      : ('param_init_lstm'        , 'lstm_layer'),
-                'lstm_cond' : ('param_init_lstm_cond'   , 'lstm_cond_layer'),
+                'ff'                : ('param_init_fflayer'     , 'fflayer'),
+                'gru'               : ('param_init_gru'         , 'gru_layer'),
+                'gru_cond'          : ('param_init_gru_cond'    , 'gru_cond_layer'),
+                'gru_cond_multi'    : ('param_init_gru_cond'    , 'gru_cond_multi_layer'),
+                'lstm'              : ('param_init_lstm'        , 'lstm_layer'),
+                'lstm_cond'         : ('param_init_lstm_cond'   , 'lstm_cond_layer'),
              }
 
     init, layer = layers[name]
@@ -136,7 +137,7 @@ def gru_layer(tparams, state_below, prefix='gru', mask=None, profile=False, mode
     if mask is None:
         # tensor.alloc(value, *shape)
         # mask: (n_steps, 1) filled with 1
-        mask = tensor.alloc(1., state_below.shape[0], 1)
+        mask = tensor.alloc(1., nsteps, 1)
 
     # state_below is the input word embeddings
     # input to the gates, concatenated
@@ -233,13 +234,13 @@ def gru_cond_layer(tparams, state_below, context, prefix='gru_cond',
     nsteps = state_below.shape[0]
 
     # Batch or single sample?
-    n_samples = 1 if state_below.ndim != 3 else state_below.shape[1]
+    n_samples = state_below.shape[1] if state_below.ndim == 3 else 1
 
     # if we have no mask, we assume all the inputs are valid
     # tensor.alloc(value, *shape)
     # mask: (n_steps, 1) filled with 1
     if mask is None:
-        mask = tensor.alloc(1., state_below.shape[0], 1)
+        mask = tensor.alloc(1., nsteps, 1)
 
     # Infer RNN dimensionality
     dim = tparams[_p(prefix, 'Wcx')].shape[1]
@@ -390,6 +391,200 @@ def gru_cond_layer(tparams, state_below, context, prefix='gru_cond',
                                     mode=mode,
                                     strict=True)
     return rval
+
+###########################################################
+# Conditional GRU layer with multiple context and attention
+###########################################################
+def gru_cond_multi_layer(tparams, state_below, ctx1, ctx2, prefix='gru_cond_multi',
+                         input_mask=None, one_step=False,
+                         init_state=None, ctx1_mask=None,
+                         profile=False, mode=None):
+    if one_step:
+        assert init_state, 'previous state must be provided'
+
+    # Context
+    # n_timesteps x n_samples x ctxdim
+    assert ctx1 and ctx2, 'Contexts must be provided'
+    assert ctx1.ndim == 3 and ctx2.ndim == 3, 'Contexts must be 3-d: #annotation x #sample x dim'
+
+    # Number of padded source timesteps
+    nsteps = state_below.shape[0]
+
+    # Batch or single sample?
+    n_samples = state_below.shape[1] if state_below.ndim == 3 else 1
+
+    # if we have no mask, we assume all the inputs are valid
+    # tensor.alloc(value, *shape)
+    # input_mask: (n_steps, 1) filled with 1
+    if input_mask is None:
+        input_mask = tensor.alloc(1., nsteps, 1)
+
+    # Infer RNN dimensionality
+    dim = tparams[_p(prefix, 'Wcx')].shape[1]
+
+    # initial/previous state
+    # if not given, assume it's all zeros
+    if init_state is None:
+        init_state = tensor.alloc(0., n_samples, dim)
+
+    # These two dot products are same with gru_layer, refer to the equations.
+    # [W_r * X + b_r, W_z * X + b_z]
+    state_below_ = tensor.dot(state_below, tparams[_p(prefix, 'W')]) + tparams[_p(prefix, 'b')]
+
+    # input to compute the hidden state proposal
+    # This is the [W*x]_j in the eq. 8 of the paper
+    state_belowx = tensor.dot(state_below, tparams[_p(prefix, 'Wx')]) + tparams[_p(prefix, 'bx')]
+
+    # Wc_att: dimctx -> dimctx
+    # Linearly transform the contexts to another space with same dimensionality
+    pctx1_ = tensor.dot(ctx1, tparams[_p(prefix, 'Wc_att')]) + tparams[_p(prefix, 'b_att')]
+    pctx2_ = tensor.dot(ctx2, tparams[_p(prefix, 'Wc_att')]) + tparams[_p(prefix, 'b_att')]
+
+    # Step function for the recurrence/scan
+    # Sequences
+    # ---------
+    # m_    : mask
+    # x_    : state_below_
+    # xx_   : state_belowx
+    # outputs_info
+    # ------------
+    # h_     : init_state,
+    # ctx_   : need to be defined as it's returned by _step
+    # alpha1_: need to be defined as it's returned by _step
+    # alpha2_: need to be defined as it's returned by _step
+    # non sequences
+    # -------------
+    # pctx1_ : pctx1_
+    # pctx2_ : pctx2_
+    # cc1_   : ctx1
+    # cc2_   : ctx2
+    # and all the shared weights and biases..
+    def _step(m_, x_, xx_,
+              h_, ctx_, alpha1_, alpha2_, # These ctx and alpha's are not used in the computations
+              pctx1_, pctx2_, cc1_, cc2_, U, Wc, W_comb_att, U_att, c_att, Ux, Wcx, U_nl, Ux_nl, b_nl, bx_nl):
+
+        # Do a step of classical GRU
+        h1 = gru_step(m_, x_, xx_, h_, U, Ux)
+
+        ###########
+        # Attention
+        ###########
+        # h1 X W_comb_att
+        # W_comb_att: dim -> dimctx
+        # pstate_ should be 2D as we're working with unrolled timesteps
+        pstate_ = tensor.dot(h1, W_comb_att)
+
+        # Accumulate in pctx*__ and apply tanh()
+        # This becomes the projected context(s) + the current hidden state
+        # of the decoder, e.g. this is the information accumulating
+        # into the returned original contexts with the knowledge of target
+        # sentence decoding.
+        pctx1__ = tanh(pctx1_ + pstate_[None, :, :])
+        pctx2__ = tanh(pctx2_ + pstate_[None, :, :])
+
+        # Affine transformation for alpha* = (pctx*__ X U_att) + c_att
+        # We're now down to scalar alpha's for each accumulated
+        # context (0th dim) in the pctx*__
+        # alpha1 should be n_timesteps, 1, 1
+        alpha1 = tensor.dot(pctx1__, U_att) + c_att
+        alpha2 = tensor.dot(pctx2__, U_att) + c_att
+
+        # Drop the last dimension, e.g. (n_timesteps, 1)
+        alpha1 = alpha1.reshape([alpha1.shape[0], alpha1.shape[1]])
+        alpha2 = alpha2.reshape([alpha2.shape[0], alpha2.shape[1]])
+
+        # Exponentiate alpha1
+        alpha1 = tensor.exp(alpha1)
+        alpha2 = tensor.exp(alpha2)
+
+        # If there is a context mask, multiply with it to cancel unnecessary steps
+        # We won't have a ctx_mask for image vectors
+        if ctx1_mask:
+            alpha1 = alpha1 * ctx1_mask
+
+        # Normalize so that the sum makes 1
+        alpha1 = alpha1 / (alpha1.sum(0, keepdims=True) + 1e-6)
+        alpha2 = alpha2 / (alpha2.sum(0, keepdims=True) + 1e-6)
+
+        # Compute the current context ctx*_ as the alpha-weighted sum of
+        # the initial contexts ctx*'s
+        ctx1_ = (cc1_ * alpha1[:, :, None]).sum(0)
+        ctx2_ = (cc2_ * alpha2[:, :, None]).sum(0)
+
+        # Sum the weighted-contexts and apply tanh()
+        ctx_ = tanh(ctx1_ + ctx2_)
+
+        ############################################
+        # ctx*_ and alpha computations are completed
+        ############################################
+
+        ####################################
+        # The below code is another GRU cell
+        ####################################
+        # Affine transformation: h1 X U_nl + b_nl
+        # U_nl, b_nl: Stacked dim*2
+        preact = tensor.dot(h1, U_nl) + b_nl
+
+        # Transform the weighted context sum with Wc
+        # and add it to preact
+        # Wc: dimctx -> Stacked dim*2
+        preact += tensor.dot(ctx_, Wc)
+
+        # Apply sigmoid nonlinearity
+        preact = sigmoid(preact)
+
+        # Slice activations: New gates r2 and u2
+        r2 = _tensor_slice(preact, 0, dim)
+        u2 = _tensor_slice(preact, 1, dim)
+
+        preactx = (tensor.dot(h1, Ux_nl) + bx_nl) * r2
+        preactx += tensor.dot(ctx_, Wcx)
+
+        # Candidate hidden
+        h2_tilda = tanh(preactx)
+
+        # Leaky integration between the new h2 and the
+        # old h1 computed in line 285
+        h2 = u2 * h2_tilda + (1. - u2) * h1
+        h2 = m_[:, None] * h2 + (1. - m_)[:, None] * h1
+
+        return h2, ctx_, alpha1.T, alpha2.T
+
+    # Sequences are the input mask and the transformed target embeddings
+    seqs = [input_mask, state_below_, state_belowx]
+
+    # Create a list of shared parameters for easy parameter passing
+    shared_vars = [tparams[_p(prefix, 'U')],
+                   tparams[_p(prefix, 'Wc')],
+                   tparams[_p(prefix, 'W_comb_att')],
+                   tparams[_p(prefix, 'U_att')],
+                   tparams[_p(prefix, 'c_att')],
+                   tparams[_p(prefix, 'Ux')],
+                   tparams[_p(prefix, 'Wcx')],
+                   tparams[_p(prefix, 'U_nl')],
+                   tparams[_p(prefix, 'Ux_nl')],
+                   tparams[_p(prefix, 'b_nl')],
+                   tparams[_p(prefix, 'bx_nl')]]
+
+    if one_step:
+        rval = _step(*(seqs + [init_state, None, None, None, pctx1_, pctx2_, ctx1, ctx2] + shared_vars))
+    else:
+        outputs_info=[init_state,
+                      tensor.alloc(0., n_samples, ctx1.shape[2]), # ctxdim       (ctx_)
+                      tensor.alloc(0., n_samples, ctx1.shape[0]), # n_timesteps  (alpha1)
+                      tensor.alloc(0., n_samples, ctx2.shape[0])] # n_timesteps  (alpha2)
+
+        rval, updates = theano.scan(_step,
+                                    sequences=seqs,
+                                    outputs_info=outputs_info,
+                                    non_sequences=[pctx1_, pctx2_, ctx1, ctx2] + shared_vars,
+                                    name=_p(prefix, '_layers'),
+                                    n_steps=nsteps,
+                                    profile=profile,
+                                    mode=mode,
+                                    strict=True)
+    return rval
+
 
 #################
 # LSTM (from SAT)
