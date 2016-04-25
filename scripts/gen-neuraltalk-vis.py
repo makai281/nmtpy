710f63abfc11c71c79db5de26c6a73fd15ca4868
Ozan Caglayan <ozancag@gmail.com>
2016-04-25 12:55:24 +0200

    update

--- scripts/gen-neuraltalk-vis.py
+++ scripts/gen-neuraltalk-vis.py
@@ -15,11 +15,17 @@ if __name__ == '__main__':
     with open(imglist) as f:
         imglist = f.read().strip().split("\n")
 
-    systems = []
+    metadata = []
     hyps = []
     for fname in sys.argv[2:]:
         print "Reading %s" % fname
-        systems.append(os.path.basename(fname).split(".")[0])
+        sysname = os.path.basename(fname).rsplit(".", 2)[0]
+        score = open('%s.score' % fname).read().strip()
+        d = eval(score)
+        d = dict([(k.upper().replace("_", ""), "%.2f" % (100*float(v))) for k,v \
+                    in d.iteritems() if k.startswith(("Bleu", "METEOR"))])
+        d['name'] = sysname
+        metadata.append(d)
 
         with open(fname) as f:
             hyps.append(f.read().strip().split("\n"))
@@ -35,7 +41,7 @@ if __name__ == '__main__':
     for hyp, img in izip(hyps, imglist):
         dump.append({'image_id' : img, 'sents' : hyp})
 
-    final = {'captions' : dump, 'metadata' : {}}
+    final = {'captions' : dump, 'metadata' : metadata}
 
     with open('vis.json', 'w') as f:
         json.dump(final, f)
