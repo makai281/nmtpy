f2820c71d292aa73e38ab2bff7c8ef635f991d61
Ozan Caglayan <ozancag@gmail.com>
2016-06-12 00:31:24 +0200

    attention: get attention weights

--- nmtpy/models/attention.py
+++ nmtpy/models/attention.py
@@ -301,7 +301,7 @@ class Model(BaseModel):
         # compile a function to do the whole thing above
         # next hidden state to be used
         inputs = [y, ctx, init_state]
-        outs = [next_log_probs, next_word, next_state]
+        outs = [next_log_probs, next_word, next_state, alphas]
         self.f_next = theano.function(inputs, outs, name='f_next', profile=self.profile)
 
     def beam_search(self, inputs, beam_size=12, maxlen=50, suppress_unks=False):
@@ -344,7 +344,7 @@ class Model(BaseModel):
             # of duplicated left hypotheses. tiled_ctx is always the same except
             # the 2nd dimension as the context vectors of the source sequence
             # is always the same regardless of the decoding step.
-            next_log_p, _, next_state = self.f_next(*[next_w, tiled_ctx, next_state])
+            next_log_p, _, next_state, alphas = self.f_next(*[next_w, tiled_ctx, next_state])
 
             if suppress_unks:
                 next_log_p[:, 1] = -np.inf
