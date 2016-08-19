f430ccff562ebc8e733bf7da4aafb10a56e3515b
Ozan Caglayan <ozancag@gmail.com>
2016-08-19 13:12:07 +0200

    disable not-ready iterators

--- nmtpy/iterators/__init__.py
+++ nmtpy/iterators/__init__.py
@@ -1,17 +1,13 @@
-from wmt            import WMTIterator
-from sqlite         import SQLIterator
+#from wmt            import WMTIterator
+#from sqlite         import SQLIterator
 from text           import TextIterator
-from flickr         import FlickrIterator
+#from flickr         import FlickrIterator
 from bitext         import BiTextIterator
-from homogeneouswmt import WMTHomogeneousIterator
+#from homogeneouswmt import WMTHomogeneousIterator
 
 def get_iterator(name):
     iters = {
                 "text"      : TextIterator,
                 "bitext"    : BiTextIterator,
-                "flickr"    : FlickrIterator,
-                "wmt"       : WMTIterator,
-                "sqlite"    : SQLIterator,
-                "hwmt"      : WMTHomogeneousIterator,
             }
     return iters[name]
