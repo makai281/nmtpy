7169983fcb2492c254e7fd8258e5a1b05aa53286
Ozan Çağlayan <ozancag@gmail.com>
2016-08-19 14:14:43 +0300

    wmt: update

--- nmtpy/iterators/wmt.py
+++ nmtpy/iterators/wmt.py
@@ -13,139 +13,85 @@ import numpy as np
 from ..nmtutils import sent_to_idx
 from ..typedef  import INT, FLOAT
 from .iterator import Iterator
+from .homogeneous import HomogeneousData
 
+# This is an iterator specifically to be used by the .pkl
+# corpora files created for WMT16 Shared Task on Multimodal Machine Translation
 # Each element of the list that is pickled is in the following format:
-# [ssplit, tsplit, imgid, imgname, swords, twords]
+# [src_split_idx, trg_split_idx, imgid, imgname, src_words, trg_words]
 
-class WMTIterator(object):
-    def __init__(self, batch_size,
-                 pkl_file,
-                 img_feats_file=None,
-                 trg_dict=None, src_dict=None,
-                 n_words_trg=0, n_words_src=0,
-                 mode='all',
-                 shuffle=False):
+class WMTIterator(Iterator):
+    def __init__(self, batch_size, seed=1234, mask=True, shuffle_mode=None, **kwargs):
+        super(WMTIterator, self).__init__(batch_size, seed, mask, shuffle_mode)
 
-        self.n_samples = 0
+        assert 'pklfile' in kwargs, "Missing argument pklfile"
+        assert 'imgfile' in kwargs, "Missing argument imgfile"
+        assert 'srcdict' in kwargs, "Missing argument srcdict"
 
-        # These are set after reading the pkl file
-        self.src_avail = False
-        self.trg_avail = False
-        self.img_avail = False
+        # Short-list sizes
+        self.n_words_src = kwargs.get('n_words_src', 0)
+        self.n_words_trg = kwargs.get('n_words_trg', 0)
 
-        # For minibatch shuffling
-        random.seed(1234)
+        # How do we refer to symbolic data variables?
+        self.src_name = kwargs.get('src_name', 'x')
+        self.trg_name = kwargs.get('trg_name', 'y')
 
-        # pkl file which contains a list of Sample objects
-        self.pkl_file = pkl_file
+        # How do we use the multimodal data?
+        # 'all'     : All combinations (~725K parallel)
+        # 'single'  : Take only the first pair e.g., train0.en->train0.de (~29K parallel)
+        # 'pairs'   : Take only one-to-one pairs e.g., train_i.en->train_i.de (~145K parallel)
+        self.mode = kwargs.get('mode', 'all')
 
-        # This is expected to be a .npy file
-        self.img_feats_file = img_feats_file
-        self.img_feats = None
-
-        # Target word dictionary and short-list limit
-        # This may not be available during validation
-        self.trg_dict = trg_dict
-        self.n_words_trg = n_words_trg
+        # pkl file which contains a list of samples
+        self.pklfile = kwargs['pklfile']
+        # Resnet-50 image features file
+        self.imgfile = kwargs['imgfile']
 
         # Source word dictionary and short-list limit
-        # This may not be available if the task is image -> description
-        self.src_dict = src_dict
-        self.n_words_src = n_words_src
+        # This may not be available if the task is image -> description (Not implemented)
+        self.srcdict = kwargs['srcdict']
+        # This may not be available during validation
+        self.trgdict = kwargs.get('trgdict', None)
 
-        # Batch size
-        self.batch_size = batch_size
+        self._keys = [self.src_name]
+        if self.mask:
+            self._keys.append("%s_mask" % self.src_name)
 
-        # Whether to shuffle after each epoch
-        self.shuffle = shuffle
+        # We have images in the middle
+        self._keys.append("%s_img" % self.src_name)
 
-        # 'all'     : Use everything available in the pkl file (default)
-        # 'single'  : Take only the first pair e.g., train0.en->train0.de
-        # 'pairs'   : Take only one-to-one pairs e.g., train_i.en->train_i.de
-        self.mode = mode
-
-        # keys define what to return during iteration
-        self.__keys = []
-        if self.src_dict:
-            # We have source sentences
-            self.__keys.extend(["x", "x_mask"])
-
-        if self.img_feats_file:
-            # We have source images
-            self.__keys.append("x_img")
-
-        if self.trg_dict:
-            # We have target sentences
-            self.__keys.extend(["y", "y_mask"])
-
-        # Read the data
-        self.read()
-
-    def __len__(self):
-        return self.n_samples
-
-    def set_batch_size(self, bs):
-        """Sets the batch size and recreates batch idxs."""
-        self.batch_size = bs
-
-        if self.batch_size == 1:
-            self.__keys = [k for k in self.__keys if not k.endswith("_mask")]
-
-        # Create batch idxs
-        self.__batches = [xrange(i, min(i+self.batch_size, self.n_samples)) \
-                            for i in range(0, self.n_samples, self.batch_size)]
-        self.__batch_iter = iter(self.__batches)
-
-    def rewind(self):
-        """Reshuffle if requested."""
-        self.__batch_iter = iter(self.__batches)
-        if self.shuffle:
-            random.shuffle(self.samples)
-
-    def __iter__(self):
-        return self
-
-    def prepare_batches(self):
-        pass
+        # Target may not be available during validation
+        if self.trgdict:
+            self._keys.append(self.trg_name)
+            if self.mask:
+                self._keys.append("%s_mask" % self.trg_name)
 
     def read(self):
-        if self.img_feats_file:
-            self.img_feats = np.load(self.img_feats_file)
+        # Load image features file
+        self.img_feats = np.load(self.imgfile)
 
-            # NOTE: Hacky check to distinguish btw resnet and VGG
-            if self.img_feats.ndim == 2:
-                # Transpose and fix dimensionality of convolutional patches
-                # for VGG
-                self.img_feats.shape = (self.img_feats.shape[0], 512, 14, 14)
-                self.img_feats.shape = (self.img_feats.shape[0], 512, 196)
-                self.img_feats = self.img_feats.transpose(0, 2, 1)
-
-        # Load the samples
-        with open(self.pkl_file, 'rb') as f:
-            # This import needs to be here so that unpickling works correctly
-            self.samples = cPickle.load(f)
+        # Load the corpora
+        with open(self.pklfile, 'rb') as f:
+            self._seqs = cPickle.load(f)
 
         # Check for what is available
-        ss = self.samples[0]
-        if ss[0] is not None and self.src_dict:
-            self.src_avail = True
-        if ss[1] is not None and self.trg_dict:
+        ss = self._seqs[0]
+        if ss[1] is not None and self.trgdict:
             self.trg_avail = True
-        if ss[2] is not None and self.img_feats is not None:
-            self.img_avail = True
 
         if self.mode == 'single':
             # Just take the first src-trg pair. Useful for validation
-            if ss[0] is not None and ss[1] is not None:
-                self.samples = [s for s in self.samples if (s[0] == s[1] == 0)]
-            elif self.src_avail:
-                self.samples = [s for s in self.samples if (s[0] == 0)]
+            if ss[1] is not None:
+                self._seqs = [s for s in self._seqs if (s[0] == s[1] == 0)]
+            else:
+                self._seqs = [s for s in self._seqs if (s[0] == 0)]
+
         elif ss[1] is not None and self.mode == 'pairs':
             # Take the pairs with split idx's equal
-            self.samples = [s for s in self.samples if s[0] == s[1]]
+            self._seqs = [s for s in self._seqs if s[0] == s[1]]
 
         # We now have a list of samples
-        self.n_samples = len(self.samples)
+        self.n_samples = len(self._seqs)
 
         # Some statistics
         unk_trg = 0
@@ -154,49 +100,86 @@ class WMTIterator(object):
         total_trg_words = []
 
         # Let's map the sentences once to idx's
-        if self.src_avail or self.trg_avail:
-            for sample in self.samples:
-                if self.src_avail:
-                    sample[4] = sent_to_idx(self.src_dict, sample[4], self.n_words_src)
-                    total_src_words.extend(sample[4])
-                if self.trg_avail:
-                    sample[5] = sent_to_idx(self.trg_dict, sample[5], self.n_words_trg)
-                    total_trg_words.extend(sample[5])
+        for sample in self._seqs:
+            sample[4] = sent_to_idx(self.src_dict, sample[4], self.n_words_src)
+            total_src_words.extend(sample[4])
+            if self.trg_avail:
+                sample[5] = sent_to_idx(self.trg_dict, sample[5], self.n_words_trg)
+                total_trg_words.extend(sample[5])
 
         self.unk_src = total_src_words.count(1)
         self.unk_trg = total_trg_words.count(1)
         self.total_src_words = len(total_src_words)
         self.total_trg_words = len(total_trg_words)
 
-        self.set_batch_size(self.batch_size)
+        if self.shuffle_mode == 'trglen':
+            # Homogeneous batches ordered by target sequence length
+            self._iter = HomogeneousData(self._seqs, self.batch_size, 5)
+        elif self.shuffle_mode == 'simple':
+            # Simple shuffle
+            self._idxs = np.random.permutation(self.n_samples)
+            self.prepare_batches()
+        else:
+            # Ordered
+            self._idxs = np.arange(self.n_samples)
+            self.prepare_batches()
 
-    def next(self):
-        try:
-            # Get batch idxs
-            idxs = next(self.__batch_iter)
-            x = x_img = x_mask = y = y_mask = None
+    def prepare_batches(self):
+        self._minibatches = []
 
-            # Target sentence
-            if self.trg_avail:
-                y, y_mask = Iterator.mask_data([self.samples[i][5] for i in idxs])
-
-            # Optional source sentences
-            if self.src_avail:
-                x, x_mask = Iterator.mask_data([self.samples[i][4] for i in idxs])
+        for i in range(0, self.n_samples, self.batch_size):
+            batch_idxs = self._idxs[i:i + self.batch_size]
 
             # Source image features
-            if self.img_avail:
-                img_idxs = [self.samples[i][2] for i in idxs]
-                # Do this 196 x bsize x 512
-                x_img = self.img_feats[img_idxs].transpose(1, 0, 2)
-                if self.batch_size == 1:
-                    # Drop middle axis
-                    x_img = x_img.squeeze()
+            img_idxs = [self._seqs[i][2] for i in batch_idxs]
 
-            return OrderedDict([(k, locals()[k]) for k in self.__keys if locals()[k] is not None])
-        except StopIteration as si:
-            self.rewind()
-            raise
+            # Do this 196 x bsize x 512
+            x_img = self.img_feats[img_idxs].transpose(1, 0, 2)
+
+            data_and_mask = Iterator.mask_data([self._seqs[i][4] for i in batch_idxs])
+            data_and_mask += x_img
+            if self.trg_avail:
+                data_and_mask += Iterator.mask_data([self._seqs[i][5] for i in batch_idxs])
+
+            # TODO: We should handle this in the model
+            #if self.batch_size == 1:
+               ## Drop middle axis
+               #x_img = x_img.squeeze()
+
+            self._minibatches.append(data_and_mask)
+
+        self.rewind()
+
+    def rewind(self):
+        if self.shuffle_mode != 'trglen':
+            self._iter = iter(self._minibatches)
+
+#    def next(self):
+        #try:
+            ## Get batch idxs
+            #idxs = next(self.__batch_iter)
+            #x = x_img = x_mask = y = y_mask = None
+
+            ## Target sentence
+            #if self.trg_avail:
+                #y, y_mask = Iterator.mask_data([self._seqs[i][5] for i in idxs])
+
+            ## Optional source sentences
+            #if self.src_avail:
+                #x, x_mask = Iterator.mask_data([self._seqs[i][4] for i in idxs])
+
+            ## Source image features
+            #img_idxs = [self._seqs[i][2] for i in idxs]
+            ## Do this 196 x bsize x 512
+            #x_img = self.img_feats[img_idxs].transpose(1, 0, 2)
+            #if self.batch_size == 1:
+                ## Drop middle axis
+                #x_img = x_img.squeeze()
+
+            #return OrderedDict([(k, locals()[k]) for k in self.__keys if locals()[k] is not None])
+        #except StopIteration as si:
+            #self.rewind()
+            #raise
 
 if __name__ == '__main__':
     from nmtpy.nmtutils import load_dictionary
