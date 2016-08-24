7328b26765b5fafa02c76602dcb50535872eea04
Ozan Caglayan <ozancag@gmail.com>
2016-08-24 17:13:37 +0200

    hypersearch kinda works

--- nmtpy/hypersearch.py
+++ nmtpy/hypersearch.py
@@ -7,27 +7,26 @@ from nmtpy.nmtutils import DotDict
 
 # How many runs?
 N_ITER      = 20
-MAX_EPOCHS  = 5
+MAX_EPOCHS  = 10
 GCLIP       = 1.0
 
 def hypersearch(model, log, args):
     results = []
 
-    # One time initialization of the model
-    log.info("Loading data")
-    model.load_data()
-    log.info("Initializing parameters")
-    model.init_params()
-    model.init_shared_variables()
-    log.info("Number of parameters: %s" % model.get_nb_params())
-    log.info("Building model")
-    cost = model.build()
-
-    # Enable dropout in training if any
-    model.set_dropout(True)
-
-    l2_factors = 10**np.random.uniform(-4, -7, N_ITER)
+    l2_factors = 10**(-np.arange(5,9).astype('float64'))
     for l2 in l2_factors:
+        # Set seed
+        np.random.seed(1234)
+
+        log.info("Loading data")
+        model.load_data()
+        log.info("Initializing parameters")
+        model.init_params()
+        model.init_shared_variables()
+        log.info("Number of parameters: %s" % model.get_nb_params())
+        log.info("Building model")
+        cost = model.build()
+
         args.decay_c = l2
         # Get regularized training cost
         cost = model.get_regularized_cost(cost, args.decay_c, 0)
@@ -36,9 +35,12 @@ def hypersearch(model, log, args):
         log.info("Building optimizer with regularized cost (l2: %.10f)" % l2)
         model.build_optimizer(cost, GCLIP, debug=False)
 
+        # Enable dropout in training if any
+        model.set_dropout(True)
+
         # Create mainloop
         loop = MainLoop(model, log, args)
-        loop.beam_size = 2
+        loop.beam_size = 12
         loop.save_best = False
         loop.f_verbose = 100
         loop.max_epochs = MAX_EPOCHS
