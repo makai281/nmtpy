cd628d69086b11648619f3f5c176f5d7cec68f90
Ozan Caglayan <ozancag@gmail.com>
2016-09-05 21:36:08 +0200

    filters: add BPE post-processing filter

--- nmtpy/filters.py
+++ nmtpy/filters.py
@@ -12,8 +12,24 @@ class CompoundFilter(object):
         else:
             return [self.__filter(e) for e in inp]
 
+class BPEFilter(object):
+    """Filters out fillers from BPE applied sentences."""
+    def __init__(self):
+        pass
+
+    def __filter(self, s):
+        return s.replace("@@ ", "")
+
+    def __call__(self, inp):
+        if isinstance(inp, str):
+            return self.__filter(inp)
+        else:
+            return [self.__filter(e) for e in inp]
+
+
 def get_filter(name):
     filters = {
                 "compound"     : CompoundFilter(),
+                "bpe"          : BPEFilter(),
               }
     return filters.get(name, None)
