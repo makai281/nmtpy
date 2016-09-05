a0d05a69cabd65e0fe1ffac3c4a2a4b0b6856a81
Ozan Caglayan <ozancag@gmail.com>
2016-09-05 15:49:40 +0200

    fusionlayers: numerically stable exp

--- nmtpy/models/fusionlayers.py
+++ nmtpy/models/fusionlayers.py
@@ -113,8 +113,8 @@ def multigru_concat_ind_ind(tparams, state_below,
         alpha2 = alpha2.reshape([alpha2.shape[0], alpha2.shape[1]])
 
         # Exponentiate alpha1
-        alpha1 = tensor.exp(alpha1)
-        alpha2 = tensor.exp(alpha2)
+        alpha1 = tensor.exp(alpha1 - alpha1.max(0, keepdims=True))
+        alpha2 = tensor.exp(alpha2 - alpha2.max(0, keepdims=True))
 
         # If there is a context mask, multiply with it to cancel unnecessary steps
         # We won't have a ctx_mask for image vectors
@@ -332,8 +332,8 @@ def multigru_concat_dep_dep(tparams, state_below,
         alpha2 = alpha2.reshape([alpha2.shape[0], alpha2.shape[1]])
 
         # Exponentiate alpha1
-        alpha1 = tensor.exp(alpha1)
-        alpha2 = tensor.exp(alpha2)
+        alpha1 = tensor.exp(alpha1 - alpha1.max(0, keepdims=True))
+        alpha2 = tensor.exp(alpha2 - alpha2.max(0, keepdims=True))
 
         # If there is a context mask, multiply with it to cancel unnecessary steps
         # We won't have a ctx_mask for image vectors
@@ -551,8 +551,8 @@ def multigru_concat_dep_ind(tparams, state_below,
         alpha2 = alpha2.reshape([alpha2.shape[0], alpha2.shape[1]])
 
         # Exponentiate alpha1
-        alpha1 = tensor.exp(alpha1)
-        alpha2 = tensor.exp(alpha2)
+        alpha1 = tensor.exp(alpha1 - alpha1.max(0, keepdims=True))
+        alpha2 = tensor.exp(alpha2 - alpha2.max(0, keepdims=True))
 
         # If there is a context mask, multiply with it to cancel unnecessary steps
         # We won't have a ctx_mask for image vectors
@@ -767,8 +767,8 @@ def multigru_concat_ind_dep(tparams, state_below,
         alpha2 = alpha2.reshape([alpha2.shape[0], alpha2.shape[1]])
 
         # Exponentiate alpha1
-        alpha1 = tensor.exp(alpha1)
-        alpha2 = tensor.exp(alpha2)
+        alpha1 = tensor.exp(alpha1 - alpha1.max(0, keepdims=True))
+        alpha2 = tensor.exp(alpha2 - alpha2.max(0, keepdims=True))
 
         # If there is a context mask, multiply with it to cancel unnecessary steps
         # We won't have a ctx_mask for image vectors
