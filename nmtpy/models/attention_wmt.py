9fd46020e789961ff1848409402e2373520fa670
Ozan Caglayan <ozancag@gmail.com>
2016-09-22 09:44:45 +0200

    attention_wmt: Use all data from pkl

--- nmtpy/models/attention_wmt.py
+++ nmtpy/models/attention_wmt.py
@@ -29,7 +29,7 @@ class Model(ParentModel):
         # Call parent's init first
         super(Model, self).__init__(seed, logger, **kwargs)
 
-    def load_valid_data(self, from_translate=False, data_mode='single'):
+    def load_valid_data(self, from_translate=False, data_mode='all'):
         if from_translate:
             self.valid_ref_files = self.data['valid_trg']
             if isinstance(self.valid_ref_files, str):
@@ -40,7 +40,7 @@ class Model(ParentModel):
                     mask=False,
                     pklfile=self.data['valid_src'],
                     srcdict=self.src_dict, n_words_src=self.n_words_src,
-                    mode=data_mode)
+                    mode='all') # Override the given parameter
         else:
             # Take the first validation item for NLL computation
             self.valid_iterator = WMTIterator(
@@ -48,7 +48,7 @@ class Model(ParentModel):
                     pklfile=self.data['valid_src'],
                     trgdict=self.trg_dict, srcdict=self.src_dict,
                     n_words_trg=self.n_words_trg, n_words_src=self.n_words_src,
-                    mode='single')
+                    mode='all') # Override the given parameter
 
         self.valid_iterator.read()
 
@@ -58,7 +58,7 @@ class Model(ParentModel):
                 pklfile=self.data['train_src'],
                 trgdict=self.trg_dict, srcdict=self.src_dict,
                 n_words_trg=self.n_words_trg, n_words_src=self.n_words_src,
-                mode='pairs')
+                mode='all')
         # Prepare batches
         self.train_iterator.read()
         self.load_valid_data()
