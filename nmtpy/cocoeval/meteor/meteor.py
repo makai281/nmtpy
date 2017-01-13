6810349181664488134513063f98b026781682f2
Ozan Caglayan <ozancag@gmail.com>
2017-01-13 16:32:53 +0100

    cocoeval/meteor: remove unused method

--- nmtpy/cocoeval/meteor/meteor.py
+++ nmtpy/cocoeval/meteor/meteor.py
@@ -23,6 +23,9 @@ class Meteor:
         # Used to guarantee thread safety
         self.lock = threading.Lock()
 
+    def method(self):
+        return "METEOR"
+
     def compute_score(self, gts, res):
         imgIds = sorted(gts.keys())
         scores = []
@@ -34,39 +37,27 @@ class Meteor:
             stat = self._stat(res[i][0], gts[i])
             eval_line += ' ||| {}'.format(stat)
 
+        # Send to METEOR
         self.meteor_p.stdin.write('{}\n'.format(eval_line))
-        for i in range(len(imgIds)):
-            scores.append(float(self.meteor_p.stdout.readline().strip()))
 
-        score = float(self.meteor_p.stdout.readline().strip())
+        # Collect segment scores
+        for i in range(len(imgIds)):
+            score = float(self.meteor_p.stdout.readline().strip())
+            scores.append(score)
+
+        # Final score
+        final_score = float(self.meteor_p.stdout.readline().strip())
         self.lock.release()
 
-        return score, scores
-
-    def method(self):
-        return "METEOR"
+        return final_score, scores
 
     def _stat(self, hypothesis_str, reference_list):
-        # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
-        hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
+        hypothesis_str = hypothesis_str.replace('|||', '').replace('  ', ' ')
         score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
+        # We obtained --> SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
         self.meteor_p.stdin.write('{}\n'.format(score_line))
         return self.meteor_p.stdout.readline().strip()
 
-    def _score(self, hypothesis_str, reference_list):
-        self.lock.acquire()
-        # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
-        hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
-        score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
-        self.meteor_p.stdin.write('{}\n'.format(score_line))
-        stats = self.meteor_p.stdout.readline().strip()
-        eval_line = 'EVAL ||| {}'.format(stats)
-        # EVAL ||| stats 
-        self.meteor_p.stdin.write('{}\n'.format(eval_line))
-        score = float(self.meteor_p.stdout.readline().strip())
-        self.lock.release()
-        return score
- 
     def __del__(self):
         self.lock.acquire()
         self.meteor_p.stdin.close()
