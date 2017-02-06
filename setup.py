f3eb28ec02dd90c31d6d8a8cc8817b4bf408be99
Ozan Caglayan <ozancag@gmail.com>
2017-02-06 10:50:25 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,21 @@ pycocopackages = ['nmtpy.cocoeval.%s' % m for m in pycocometrics]
 setup(
         name='nmtpy',
         version=nmtpy.__version__,
-        description='NMT framework for Python based on Theano',
-        url='http://github.com/lium-lst/nmtpy.git',
+        description='Neural Machine Translation Framework in Python',
+        url='https://github.com/lium-lst/nmtpy',
         author='Ozan Çağlayan',
         author_email='ozancag@gmail.com',
         license='MIT',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2 :: Only',
+            'Programming Language :: Python :: 2.7',
+            'Operating System :: POSIX',
+            ],
+        keywords='nmt neural-mt translation deep-learning',
         packages=['nmtpy', 'nmtpy.models', 'nmtpy.iterators', 'nmtpy.metrics', 'nmtpy.cocoeval'] + pycocopackages,
         package_data={'' : ['external/meteor-1.5.jar', 'external/data/*gz', 'external/multi-bleu.perl']}, # data files
         install_requires=[
